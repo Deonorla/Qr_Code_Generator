37a24dcc0ed8bddafc8932f0c4d3ef9f6f9876b3
Emmanuel oluleye <91434033+Deonorla@users.noreply.github.com>
2022-05-31 10:50:52 +0100

    download

--- main.js
+++ main.js
@@ -11,13 +11,17 @@ forEach(links => {
 let btn = document.querySelector(".generate_button");
 let qr_code = document.querySelector(".qr_code");
 
+
 btn.addEventListener("click", ()=>{
   let email_input = document.querySelector("#emailInput");
   let email_subject = document.querySelector("#emailSubject");
   let message_field = document.querySelector("#messageField");
 
+
   if(email_input.value && email_subject.value && message_field.value !=  "" ){ // checks if any of the inputs is empty 
-      if(qr_code.childElementCount == 0) { // check if a qr-code already exists
+    
+      if(qr_code.childElementCount == 0) {
+         // check if a qr-code already exists
         generate(email_input,email_subject,message_field);
       } else {
         qr_code.innerHTML = "";// if a qr-code exists set it to empty
@@ -25,7 +29,10 @@ btn.addEventListener("click", ()=>{
 
   }else{
     document.querySelector(".error").style="";
-    document.querySelector(".error").innerHTML = "Invalid Input"
+    setTimeout(() =>{
+      document.querySelector(".error").innerHTML = "Invalid Input"
+    },1000)
+    
   }
   
 })
@@ -45,14 +52,14 @@ const generate = (email_input, email_subject, message_field) => {
 
     });   
 
-    let download = document.querySelector("button");
-    qr_code.appendChild(download);
+    let download = document.querySelector(".jpg");
+    // qr_code.appendChild(download);
 
-    let download_link = document.createElement("a");
+    let download_link = document.querySelector(".jpg");
     download_link.setAttribute("download", "qr_code.png");
-    download_link.innerHTML =`Download`;
+    // download_link.innerHTML =`Download`;
 
-    download.appendChild(download_link);
+    // download.appendChild(download_link);
 
     let qr_code_img = document.querySelector(".qr_code img")
     let qr_code_canvas = document.querySelector("canvas")
@@ -70,6 +77,3 @@ const generate = (email_input, email_subject, message_field) => {
     
 }
  
-// generate({
-//   value: 'https://qr-codes.vercel.app'
-// })
